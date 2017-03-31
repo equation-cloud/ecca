b43f66acd6ac029cd5b781a87bd34c0bbacea6b9
Rob Bacon <bacon@cantab.net>
2017-03-31 21:53:09 +0100

    Fixed bug parsing decimals with no leading zero

--- src/parsers/ecca-parser.spec.ts
+++ src/parsers/ecca-parser.spec.ts
@@ -21,10 +21,20 @@ describe('EccaParser: ParseString', () => {
     expect(eccaParser.ParseString('0.0').numerator).toBe(0);
     expect(eccaParser.ParseString('0.4').numerator).toBe(4);
     expect(eccaParser.ParseString('1.3').numerator).toBe(13);
+    expect(eccaParser.ParseString('345.3998').numerator).toBe(3453998);
+    expect(eccaParser.ParseString('100000.0001').numerator).toBe(1000000001);
+    expect(eccaParser.ParseString('3.1415926').numerator).toBe(31415926);
+    expect(eccaParser.ParseString('.97').numerator).toBe(97);
+    expect(eccaParser.ParseString('.00012345').numerator).toBe(12345);
   });
   it('will parse decimal strings to IElements with correct denominators', () => {
     expect(eccaParser.ParseString('0.0').denominator).toBe(10);
     expect(eccaParser.ParseString('0.4').denominator).toBe(10);
     expect(eccaParser.ParseString('1.3').denominator).toBe(10);
+    expect(eccaParser.ParseString('345.3998').denominator).toBe(10000);
+    expect(eccaParser.ParseString('100000.0001').denominator).toBe(10000);
+    expect(eccaParser.ParseString('3.1415926').denominator).toBe(10000000);
+    expect(eccaParser.ParseString('.97').denominator).toBe(100);
+    expect(eccaParser.ParseString('.00012345').denominator).toBe(100000000);
   });
 });
