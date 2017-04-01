3114107bc4ffdb1e809cd744b54b61616dbe31fb
Rob Bacon <bacon@cantab.net>
2017-04-01 21:23:11 +0100

    Added tests for multiplication

--- src/parsers/ecca-parser-operators.spec.ts
+++ src/parsers/ecca-parser-operators.spec.ts
@@ -35,4 +35,31 @@ describe('EccaParser: ParseString (operators)', () => {
     expect(ecca.Parser.ParseString('9649.23523/53825').operands[1].type).toBe('integer');
     expect(ecca.Parser.ParseString('9649.23523/0').operands[1].type).toBe('integer');
   });
+  //Multiplication
+  it('will parse multiplication to IElements of type "product"', () => {
+    expect(ecca.Parser.ParseString('1*2').type).toBe('product');
+    expect(ecca.Parser.ParseString('0*2123.2344').type).toBe('product');
+    expect(ecca.Parser.ParseString('0.3453*2.554').type).toBe('product');
+    expect(ecca.Parser.ParseString('.1121*.9759').type).toBe('product');
+    expect(ecca.Parser.ParseString('9649.23523*53825').type).toBe('product');
+    expect(ecca.Parser.ParseString('9649.23523*0').type).toBe('product');
+  });
+  it('will parse multiplication to IElements with the correct number of operands', () => {
+    expect(ecca.Parser.ParseString('1*2').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('0*2123.2344').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('0.3453*2.554').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('.1121*.9759*23').operands.length).toBe(3);
+    expect(ecca.Parser.ParseString('9649.23523*53825*.641437').operands.length).toBe(3);
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands.length).toBe(6);
+  });
+  it('will parse multiplication to IElements with the operands having the correct type', () => {
+    expect(ecca.Parser.ParseString('1*2').operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString('1*2').operands[1].type).toBe('integer');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[0].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[1].type).toBe('integer');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[2].type).toBe('integer');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[3].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[4].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523*0*45*1.0*0.777*3').operands[5].type).toBe('integer');
+  });  
 });
