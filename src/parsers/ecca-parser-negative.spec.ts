6890bad82099bdb8fdcbdff0346e5ed9a5b6253a
Rob Bacon <bacon@cantab.net>
2017-04-02 22:54:18 +0100

    Added tests for power operator

--- src/parsers/ecca-parser-negative.spec.ts
+++ src/parsers/ecca-parser-negative.spec.ts
@@ -9,7 +9,7 @@ describe('EccaParser: ParseString (negatives)', () => {
   it('will generate IElements within expressions of the correct type', () => {
     expect(ecca.Parser.ParseString('3+-3').operands[1].type).toBe('negate');
     expect(ecca.Parser.ParseString('7.2*-1.44').operands[1].type).toBe('negate');
-    expect(ecca.Parser.ParseString('-0*0').operands[0].type).toBe('negate');
+    expect(ecca.Parser.ParseString('-0-0').operands[0].type).toBe('negate');
     expect(ecca.Parser.ParseString('1/-0.8').operands[1].type).toBe('negate');
   })
 });
