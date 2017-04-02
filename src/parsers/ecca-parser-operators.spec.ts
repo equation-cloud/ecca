2000292c24338f951bde50253db6a653cd0a2a4d
Rob Bacon <bacon@cantab.net>
2017-04-02 22:54:18 +0100

    Added tests for power operator

--- src/parsers/ecca-parser-operators.spec.ts
+++ src/parsers/ecca-parser-operators.spec.ts
@@ -2,7 +2,39 @@ import { EccaParser } from './ecca-parser'
 import * as ecca from '../index'
 
 describe('EccaParser: ParseString (operators)', () => {
-  //Division
+  //Power
+  it('will parse exponents to IElements of type "power"', () => {
+    expect(ecca.Parser.ParseString('1^2').type).toBe('power');
+    expect(ecca.Parser.ParseString('0^2123.2344').type).toBe('power');
+    expect(ecca.Parser.ParseString('0.3453^2.554').type).toBe('power');
+    expect(ecca.Parser.ParseString('.1121^.9759').type).toBe('power');
+    expect(ecca.Parser.ParseString('9649.23523^53825').type).toBe('power');
+    expect(ecca.Parser.ParseString('9649.23523^0').type).toBe('power');
+  });
+  it('will parse exponents to IElements with two operands', () => {
+    expect(ecca.Parser.ParseString('1^2').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('0^2123.2344').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('0.3453^2.554').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('.1121^.9759').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('9649.23523^53825').operands.length).toBe(2);
+    expect(ecca.Parser.ParseString('9649.23523^0').operands.length).toBe(2);
+  });
+  it('will parse exponents to IElements with the first operand having the correct type', () => {
+    expect(ecca.Parser.ParseString('1^2').operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString('0^2123.2344').operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString('0.3453^2.554').operands[0].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('.1121^.9759').operands[0].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523^53825').operands[0].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523^0').operands[0].type).toBe('fractional');
+  });
+  it('will parse exponents to IElements with the second operand having the correct type', () => {
+    expect(ecca.Parser.ParseString('1^2').operands[1].type).toBe('integer');
+    expect(ecca.Parser.ParseString('0^2123.2344').operands[1].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('0.3453^2.554').operands[1].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('.1121^.9759').operands[1].type).toBe('fractional');
+    expect(ecca.Parser.ParseString('9649.23523^53825').operands[1].type).toBe('integer');
+    expect(ecca.Parser.ParseString('9649.23523^0').operands[1].type).toBe('integer');
+  });//Division
   it('will parse division to IElements of type "division"', () => {
     expect(ecca.Parser.ParseString('1/2').type).toBe('division');
     expect(ecca.Parser.ParseString('0/2123.2344').type).toBe('division');
