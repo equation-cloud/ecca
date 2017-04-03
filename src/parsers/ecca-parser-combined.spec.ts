8963214f5006e7a74a06150d1a84b1c162d3b45f
Rob Bacon <bacon@cantab.net>
2017-04-03 21:36:29 +0100

    Added second combined test

--- src/parsers/ecca-parser-combined.spec.ts
+++ src/parsers/ecca-parser-combined.spec.ts
@@ -2,7 +2,7 @@ import { EccaParser } from './ecca-parser'
 import * as ecca from '../index'
 
 describe('EccaParser: ParseString (operators)', () => {
-  it('will parser "5.35=1+.3*4.5+3" correctly', () => {
+  it('will parse "5.35=1+.3*4.5+3" correctly', () => {
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").type).toBe('equals');
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").operands.length).toBe(2);
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").operands[0].type).toBe('fractional');
@@ -22,5 +22,32 @@ describe('EccaParser: ParseString (operators)', () => {
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").operands[1].operands[1].operands[1].denominator).toBe(10);
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").operands[1].operands[2].type).toBe('integer');
     expect(ecca.Parser.ParseString("5.35=1+.3*4.5+3").operands[1].operands[2].value).toBe(3);
+  });
+  it('will parse "1+5^3*-2=756/3.0" correctly', () => {
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").type).toBe('equals');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands.length).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].type).toBe('sum');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands.length).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[0].value).toBe(1);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].type).toBe('product');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands.length).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].type).toBe('power');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].operands.length).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].operands[0].value).toBe(5);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].operands[1].type).toBe('integer');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[0].operands[1].value).toBe(3);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[1].type).toBe('negate');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[1].operands.length).toBe(1);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[1].operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[0].operands[1].operands[1].operands[0].value).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].type).toBe('division');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands.length).toBe(2);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands[0].type).toBe('integer');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands[0].value).toBe(756);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands[1].type).toBe('fractional');
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands[1].numerator).toBe(30);
+    expect(ecca.Parser.ParseString("1+5^3*-2=756/3.0").operands[1].operands[1].denominator).toBe(10);
   });
 });
