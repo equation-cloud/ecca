fa578bfe82c72c778cfa85513df2367a85d247d1
Rob Bacon <bacon@cantab.net>
2017-04-06 23:04:54 +0100

    Removed accidental TeX output code

--- src/index.ts
+++ src/index.ts
@@ -17,26 +17,4 @@ export class Expression {
         throw 'Unknown type for constructing Expression: ' + typeof input
     }
   }
-
-  private constructString(element : IElement, dictionary : any) : string {
-    if (element.operands) {
-      let returnValue = ""
-      element.operands.forEach(operand => {
-        returnValue.concat(this.constructString(operand, dictionary))
-      })
-      return returnValue
-    }
-    if (dictionary[element.type]) {
-      return dictionary[element.type]()
-    }
-  }
-
-  get rawTeX() : string {
-    let subst = {
-      'identifier' : (element : IElement) => element.identifier,
-      'integer' : (element : IElement) => element.value,
-      'fractional' : (element : IElement) => 
-    }
-    return this.constructString(this.ElementTree, subst);
-  }
 }
