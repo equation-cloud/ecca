6c73c29eb393dffd5eced3c8c346d3db5a4fed4c
Rob Bacon <bacon@cantab.net>
2017-05-04 20:32:13 +0100

    Moved Expression class to seperate file

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import { IParser } from './parser'
 import { IElement } from './elements'
-import { EccaParser } from './parsers/ecca-parser'
 import { convertElement } from './tex-output'
+import { Expression } from './expression'
 
-export class Expression {
-  private parser : IParser = new EccaParser()
-  public ElementTree : IElement = null
-
-  constructor (input? : string){
-    if (!input) //to allow changing parser etc.
-      return;
-    switch(typeof input) {
-      case 'string' :
-        this.ElementTree = this.parser.ParseString(input);
-        break;
-      default :
-        throw 'Unknown type for constructing Expression: ' + typeof input
-    }
-  }
-}
+export { Expression }
 
 export function generateRawTeX(input : Expression | IElement) : string {
   if (input instanceof Expression) {
