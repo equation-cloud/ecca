6219bf2fa442cf7f41327c6c010618362783d5d6
Rob Bacon <bacon@cantab.net>
2017-05-05 21:36:18 +0100

    Main simplification function
    Pass through 'identifier's, 'integer's and 'undefined' elements
    Include tests for above

--- src/simplify.spec.ts
+++ src/simplify.spec.ts
@@ -1,4 +1,26 @@
-describe('Expression : Simplification', () => {
-  it('', () => {
+import { Expression } from './expression'
+import { simplify } from './simplify'
+import { UndefinedElement } from './elements'
+
+describe('Simplification: Integer', () => {
+  it('Should pass through integer elements', () => {
+    let simplified = simplify(new Expression('1'))
+    expect(simplified.type).toBe('integer')
+    expect(simplified.value).toBe(1)
+  })
+})
+
+describe('Simplification: Identifier', () => {
+  it('Should pass through identifier elements', () => {
+    let simplified = simplify(new Expression('x'))
+    expect(simplified.type).toBe('identifier')
+    expect(simplified.identifier).toBe('x')
+  })
+})
+
+describe('Simplification: Undefined', () => {
+  it('Should pass through identifier elements', () => {
+    let simplified = simplify(new UndefinedElement())
+    expect(simplified.type).toBe('undefined')
   })
 })
--- src/simplify.ts
+++ src/simplify.ts
@@ -1,8 +1,34 @@
 import { Expression } from './expression'
-import { IElement } from './elements'
+import {
+  IElement,
+  IdentifierElement,
+  ValueElement,
+  IntegerElement,
+  FractionalElement,
+  UndefinedElement,
+  OperatorElement,
+  BracketsElement,
+  NegateElement,
+  PowerElement,
+  DivisionElement,
+  ProductElement,
+  SumElement,
+  SubtractionElement,
+  EqualsElement
+} from './elements'
 
-function simplifyElement(element : IElement) : IElement{
-  throw new Error('Not implemneted')
+function simplifyElement(element : IElement) : IElement {
+  if(element.operands) {
+    element.operands = element.operands.map(operand => simplifyElement(operand))
+  }
+  switch(element.type){
+    case 'identifier':
+    case 'integer':
+    case 'undefined':
+      return element;
+    default:
+      throw new Error('Not implemented')
+  }
 }
 
 export function simplify(input : IElement | Expression) : IElement {
