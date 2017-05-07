43745eff2297910292b3d8499cdab9917197e018
Rob Bacon <bacon@cantab.net>
2017-05-07 13:54:05 +0100

    Added tests for simplifying fractional elements
    Also changed import in test spec

--- src/simplify.spec.ts
+++ src/simplify.spec.ts
@@ -1,10 +1,10 @@
 import { Expression } from './expression'
-import { simplify } from './simplify'
+import * as simplify from './simplify'
 import { UndefinedElement } from './elements'
 
 describe('Simplification: Integer', () => {
   it('Should pass through integer elements', () => {
-    let simplified = simplify(new Expression('1'))
+    let simplified = simplify.simplify(new Expression('1'))
     expect(simplified.type).toBe('integer')
     expect(simplified.value).toBe(1)
   })
@@ -12,7 +12,7 @@ describe('Simplification: Integer', () => {
 
 describe('Simplification: Identifier', () => {
   it('Should pass through identifier elements', () => {
-    let simplified = simplify(new Expression('x'))
+    let simplified = simplify.simplify(new Expression('x'))
     expect(simplified.type).toBe('identifier')
     expect(simplified.identifier).toBe('x')
   })
@@ -20,97 +20,115 @@ describe('Simplification: Identifier', () => {
 
 describe('Simplification: Undefined', () => {
   it('Should pass through identifier elements', () => {
-    let simplified = simplify(new UndefinedElement())
+    let simplified = simplify.simplify(new UndefinedElement())
     expect(simplified.type).toBe('undefined')
     expect(simplified.value).toBeNaN()
   })
 })
 
+describe('Simplification: Fractional', () => {
+  it(('The fraction cancels out then the returned value to be an integer of the correct value'), () => {
+    let simplified = simplify.simplify(new Expression('4/2'))
+    expect(simplified.type).toBe('integer')
+    expect(simplified.value).toBe(2)
+    simplified = simplify.simplify(new Expression('9566808/4174'))
+    expect(simplified.type).toBe('integer')
+    expect(simplified.value).toBe(2292)
+  })
+  it(('The fraction simplifies then the returned value to be the simplified fraction'), () => {
+    let simplified = simplify.simplify(new Expression('7/35'))
+    expect(simplified.type).toBe('fractional')
+    expect(simplified.numerator).toBe(1)
+    expect(simplified.denominator).toBe(5)
+    simplified = simplify.simplify(new Expression('918/1326'))
+    expect(simplified.type).toBe('fractional')
+    expect(simplified.numerator).toBe(27)
+    expect(simplified.denominator).toBe(39)
+  })
+})
+
 describe('Simplification: Power (SPOW-1)', () => {
   it('should return undefined if either operand is undefined', () => {
-    expect(simplify(new Expression('1^null')).type).toBe('undefined')
-    expect(simplify(new Expression('null^x')).type).toBe('undefined')
-    expect(simplify(new Expression('null^null')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('1^null')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('null^x')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('null^null')).type).toBe('undefined')
   })
 });
 
 describe('Simplification: Power (SPOW-2)', () => {
   it('if the first operand is zero, and the second a positive integer, the result should be zero', () => {
     let expression = new Expression('0^1')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(0)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(0)
   })
   it('if the first operand is zero, and the second a positive fraction, the result should be zero', () => {
     let expression = new Expression('0^0.45')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(0)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(0)
   })
   it('if the first operand is zero, and the second zero, the result should be undefined', () => {
-    expect(simplify(new Expression('0^0')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('0^0')).type).toBe('undefined')
   })
   it('if the first operand is zero, and the second a negative integer, the result should be undefined', () => {
-    expect(simplify(new Expression('0^-5')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('0^-5')).type).toBe('undefined')
   })
   it('if the first operand is zero, and the second a negative fraction, the result should be undefined', () => {
-    expect(simplify(new Expression('0^-67.1')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('0^-67.1')).type).toBe('undefined')
   })
   it('if the first operand is zero, and the second a variable, the result should be undefined', () => {
-    expect(simplify(new Expression('0^x')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('0^x')).type).toBe('undefined')
   })
   it('if the first operand is zero, and the second bracketes, the result should be undefined', () => {
-    expect(simplify(new Expression('0^(x+2)')).type).toBe('undefined')
+    expect(simplify.simplify(new Expression('0^(x+2)')).type).toBe('undefined')
   })
 })
 
 describe('Simplification: Power (SPOW-3)', () => {
   it('should return one if the first operand is one', () => {
     let expression = new Expression('1^56')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^0.0018')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^x')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^-1')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^(x/y)')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
   })
 });
 
 describe('Simplification: Power (SPOW-3)', () => {
   it('should return one if the first operand is one', () => {
     let expression = new Expression('1^56')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^0.0018')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^x')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^-1')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
     expression = new Expression('1^(x/y)')
-    expect(simplify(expression).type).toBe('integer')
-    expect(simplify(expression).value).toBe(1)
+    expect(simplify.simplify(expression).type).toBe('integer')
+    expect(simplify.simplify(expression).value).toBe(1)
   })
 });
 
 describe('Simplification: Power (SPOW-4)', () => {
-  it('should call simplifyIntegerPower if the second operand is an integer', () => {
-    expect(null).toBe(1)
-  })
 });
 
 describe('Simplification: Power (SPOW-5)', () => {
   it('should return the original element if none of the above are true', () => {
     let expression = new Expression('y^x')
-    expect(simplify(expression)).toBe(expression.ElementTree)
+    expect(simplify.simplify(expression)).toBe(expression.ElementTree)
   })
 });
--- src/simplify.ts
+++ src/simplify.ts
@@ -17,6 +17,41 @@ import {
   EqualsElement
 } from './elements'
 
+function simplifyRationalNumber(element: FractionalElement) : IElement
+{
+}
+
+function simplifyIntegerPower(element: PowerElement) : IElement {
+  throw new Error('Not implemented')
+}
+
+function simplifyPower(element: PowerElement) : IElement {
+  //SPOW-1
+  if(element.operands[0].type === 'undefined' || element.operands[1].type == 'undefined') {
+    return new UndefinedElement();
+  }
+  //SPOW-2
+  let firstOperand = element.operands[0];
+  if (firstOperand.type === 'integer' && firstOperand.value === 0) {
+    let secondOperand = element.operands[1];
+    if (secondOperand.type === 'integer' && secondOperand.value > 0) {
+      return new IntegerElement('0');
+    } else {
+      return new UndefinedElement();
+    }
+  }
+  //SPOW-3
+  if(element.operands[0].type === 'integer' && element.operands[0].value === 1) {
+    return new IntegerElement('1');
+  }
+  //SPOW-4
+  if(element.operands[1].type === 'integer') {
+    return simplifyIntegerPower(element);
+  }
+  //SPOW-5
+  return element;
+}
+
 function simplifyElement(element : IElement) : IElement {
   if(element.operands) {
     element.operands = element.operands.map(operand => simplifyElement(operand))
@@ -24,10 +59,13 @@ function simplifyElement(element : IElement) : IElement {
   switch(element.type){
     case 'identifier':
     case 'integer':
+    case 'negate':
     case 'undefined':
       return element;
+    case 'power':
+      return simplifyPower(<PowerElement>element)
     default:
-      throw new Error('Not implemented')
+      throw new Error(element.type + ' not implemented')
   }
 }
 
